440ec2045f1b5346494015d7474b3925430e3c77
김형상 <angae1002@naver.com>
2017-03-05 20:12:40 +0900

    Update ListAdd.java

--- src/main/java/com/jayjaylab/sample/ListAdd.java
+++ src/main/java/com/jayjaylab/sample/ListAdd.java
@@ -1,85 +1,58 @@
 package com.jayjaylab.sample;
 
-import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
-
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
 @State(Scope.Thread)
 @BenchmarkMode({ Mode.AverageTime })
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-@Fork(1) 
-@Warmup(iterations=1) 
-@Measurement(iterations=1) 
-public class ListAdd {
-    int LOOP_COUNT=1000;
+public class ListAdd  {
+	int LOOP_COUNT=1000;
 	List<Integer> arrayList;
 	List<Integer> vector;
 	List<Integer> linkedList;
 	
-    @org.openjdk.jmh.annotations.State(Scope.Thread)
-    public static class State {
-        int startNumber;
-        int endNumber;
-
-        @Setup(Level.Trial)
-        public void setUp() {
-            System.out.println("setUp()");
-            startNumber = 0;
-            endNumber = 10000;
-        }
-
-        @TearDown(Level.Trial)
-        public void tearDown() {
-            System.out.println("tearDown()");
-        }
-    }
-//JMH 를 사용하여 Java SE 에 있는 List 를 구현한 클래스들의 추가/조회/삭제 기능의 성능을 비교하시오.
-//JMH 를 사용하여 Java SE 에 있는 Map을 구현한 클래스들의 추가/조회/삭제 기능의 성능을 비교하시오.
-
-    @Benchmark
-	public void addArrayList(State state, Blackhole blackhole) {
+	@GenerateMicroBenchmark
+	public void addArrayList() {
 		arrayList=new ArrayList<Integer>();
 		for(int loop=0;loop<LOOP_COUNT;loop++) {
 			arrayList.add(loop);
 		}
-		blackhole.consume(intList);
 	}
 	
-	@Benchmark
-	public void addArrayListWithInitialSize(State state, Blackhole blackhole) {
+	@GenerateMicroBenchmark
+	public void addArrayListWithInitialSize() {
 		arrayList=new ArrayList<Integer>(LOOP_COUNT);
 		for(int loop=0;loop<LOOP_COUNT;loop++) {
 			arrayList.add(loop);
 		}
-		blackhole.consume(intList);
 	}
 	
-	@Benchmark
-	public void addVector(State state, Blackhole blackhole) {
+	@GenerateMicroBenchmark
+	public void addVector() {
 		vector=new Vector<Integer>();
 		for(int loop=0;loop<LOOP_COUNT;loop++) {
 			vector.add(loop);
 		}
-		blackhole.consume(intList);
 	}
 	
-	@Benchmark
-	public void addLinkedList(State state, Blackhole blackhole) {
+	@GenerateMicroBenchmark
+	public void addLinkedList() {
 		linkedList=new LinkedList<Integer>();
 		for(int loop=0;loop<LOOP_COUNT;loop++) {
 			linkedList.add(loop);
 		}
-		blackhole.consume(intList);
 	}
-    
-    
+	
 }
+
