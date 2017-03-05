610e36cfa9e9b1989084384d468298c2af41debb
김형상 <angae1002@naver.com>
2017-03-05 20:14:23 +0900

    Update ListAdd.java

--- src/main/java/com/jayjaylab/sample/ListAdd.java
+++ src/main/java/com/jayjaylab/sample/ListAdd.java
@@ -13,9 +13,11 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
-@State(Scope.Thread)
-@BenchmarkMode({ Mode.AverageTime })
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Fork(1) 
+@Warmup(iterations=5) 
+@Measurement(iterations=5) 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class ListAdd  {
 	int LOOP_COUNT=1000;
 	List<Integer> arrayList;
