cad9aa8bf94b63731178cdad72fd4d6c809a3f86
김형상 <angae1002@naver.com>
2017-03-05 20:43:44 +0900

    Update ListAdd.java

--- src/main/java/com/jayjaylab/sample/ListAdd.java
+++ src/main/java/com/jayjaylab/sample/ListAdd.java
@@ -1,73 +1,255 @@
 package com.jayjaylab.sample;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
-import java.util.concurrent.TimeUnit;
 
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.Warmup;
-
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Fork(1) 
 @Warmup(iterations=5) 
 @Measurement(iterations=5) 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-public class ListAdd {
-	private static int LOOP_COUNT=1000;
+public class StreamBenchmark {
+    @org.openjdk.jmh.annotations.State(Scope.Thread)
+    public static class State {
+        int startNumber;
+        int endNumber;
+      
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            System.out.println("setUp()");
+            startNumber = 0;
+            endNumber = 10000;
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() {
+            System.out.println("tearDown()");
+        }
+    }
+    
+    @Benchmark
+    public void testArrayListAdd(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new ArrayList<>();
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.add(i);
+        }
+        blackhole.consume(intList);
+    }
+        
+    @Benchmark
+    public void testLinkedListAdd(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new LinkedList<>();
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.add(i);
+        }
+        blackhole.consume(intList);
+    }
+    
+    @Benchmark
+    public void testVectorAdd(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new Vector<>();
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.add(i);
+        }
+        blackhole.consume(intList);
+    }
+    
+    
+    @Benchmark
+    public void testArrayListget(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.get(0);
+        }
+        blackhole.consume(intList);
+    }
+    
+    @Benchmark
+    public void testLinkedListget(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new LinkedList<>();
+         intList.add(1);
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.get(0);
+        }
+        blackhole.consume(intList);
+    }
+    
+    @Benchmark
+    public void testVectorget(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new Vector<>();
+        intList.add(1);
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.get(0);
+        }
+        blackhole.consume(intList);
+    }
+    
+        
+    
+    @Benchmark
+    public void testArrayListremove(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new ArrayList<>();
+        
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.add(1);
+            intList.remove(0);
+        }
+        blackhole.consume(intList);
+    }
+    @Benchmark
+    public void testLinkedListremove(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new LinkedList<>();
+        
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.add(1);
+            intList.remove(0);
+        }
+        blackhole.consume(intList);
+    }
+    
+    @Benchmark
+    public void testVectorremove(State state, Blackhole blackhole) {
+        
+        List<Integer> intList = new Vector<>();
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            intList.add(1);
+            intList.remove(0);
+        }
+        blackhole.consume(intList);
+    }
+    
+    
+    @Benchmark
+    public void testHashMapAdd(State state, Blackhole blackhole) {
+        
+        Map<String, Integer> map = new HashMap<>();
+		
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            map.put("1"+i,1);
+        }
+        blackhole.consume(map);
+    }
+    
+    
+    @Benchmark
+    public void testHashTableAdd(State state, Blackhole blackhole) {
+
+		Map<String, Integer> map = new Hashtable<>();
+        
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            map.put("1"+i,1);
+        }
+        blackhole.consume(map);
+    }
+    
+    
+    @Benchmark
+    public void testTreeMapAdd(State state, Blackhole blackhole) {
+        
+		Map<String, Integer> map = new TreeMap<>();
+        
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            map.put("1"+i,1);
+        }
+        blackhole.consume(map);
+    }
 	
-	@org.openjdk.jmh.annotations.State(Scope.Thread)
-	public static class State {
-		List<String> arrayListForSearch = new ArrayList<>();
-		List<String> LinkedListForSearch = new LinkedList<>();
-
-		@Setup(Level.Trial)
-		public void setUp() {
-			
-			
-
-			for (int i = 0; i < 100; i++) {
-				arrayListForSearch.add("Hello");
-			}
-			for (int i = 0; i < 100; i++) {
-				LinkedListForSearch.add("Hello");
-			}
-		}
-	}	
-
-	@Benchmark
-	public void addArrayList() {
-		List<Integer> arrayList=new ArrayList<Integer>();
-		for(int loop=0;loop<LOOP_COUNT;loop++) {
-			arrayList.add(loop);
-		}
-	}	
-
-	@Benchmark
-	public void addVector() {
-		List<Integer> vector=new Vector<Integer>();
-		for(int loop=0;loop<LOOP_COUNT;loop++) {
-			vector.add(loop);
-		}
-	}
 	
-	@Benchmark
-	public void addLinkedList() {
-		List<Integer> linkedList=new LinkedList<Integer>();
-		for(int loop=0;loop<LOOP_COUNT;loop++) {
-			linkedList.add(loop);
-		}
-	}    
+    @Benchmark
+    public void testHashMapGet(State state, Blackhole blackhole) {
+        
+        Map<String, Integer> map = new HashMap<>();
+	map.put("1",1);	
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            map.get("1");
+        }
+        blackhole.consume(map);
+    }
+    
+    
+    @Benchmark
+    public void testHashTableGet(State state, Blackhole blackhole) {
 
+		Map<String, Integer> map = new Hashtable<>();
+        map.put("1",1);	
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            map.get("1");
+        }
+        blackhole.consume(map);
+    }
+    
+    
+    @Benchmark
+    public void testTreeMapGet(State state, Blackhole blackhole) {
+        
+		Map<String, Integer> map = new TreeMap<>();
+	map.put("1",1);	
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+            map.get("1");
+        }
+        blackhole.consume(map);
+    }
+	
+	
+    @Benchmark
+    public void testHashMapRemove(State state, Blackhole blackhole) {
+        
+        Map<String, Integer> map = new HashMap<>();
+	
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+         	map.put("1",1);	
+            	map.remove("1");
+        }
+        blackhole.consume(map);
+    }
+    
+    
+    @Benchmark
+    public void testHashTableRemove(State state, Blackhole blackhole) {
+
+	Map<String, Integer> map = new Hashtable<>();
+       
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+         	map.put("1",1);	
+            	map.remove("1");
+        }
+        blackhole.consume(map);
+    }
+    
+    
+    @Benchmark
+    public void testTreeMapRemove(State state, Blackhole blackhole) {
+        
+	Map<String, Integer> map = new TreeMap<>();
+	
+        for(int i = state.startNumber; i < state.endNumber; i++) {
+		map.put("1",1);	
+            	map.remove("1");
+        }
+        blackhole.consume(map);
+    }
 }
