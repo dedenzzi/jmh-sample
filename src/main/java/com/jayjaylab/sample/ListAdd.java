46111e71030db4c1016ae8df91c58a40678848b4
김형상 <angae1002@naver.com>
2017-03-05 20:05:44 +0900

    Update ListAdd.java

--- src/main/java/com/jayjaylab/sample/ListAdd.java
+++ src/main/java/com/jayjaylab/sample/ListAdd.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
